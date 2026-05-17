b0032de73efb217104dbfc911de2c4bd1cb38f1c
Danil Kolbasenko <danilvusockij@gmail.com>
2026-05-17 16:49:30 +0300

    Delete second block , and this fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,8 +32,8 @@ export default function Page() {
         {/* верстка первого блока и адаптация 
         это пиздец ё
         */}
- <div className="w-full mt-15 bg-[#211C24] overflow-hidden">
-  <div className="px-6 lg:px-30 py-12 lg:h-180 flex flex-col lg:flex-row items-center justify-between">
+ <div className="w-348 mb-0 mt-15 bg-[#211C24] overflow-hidden lg:w-screen">
+  <div className=" lg:px-30 py-12 lg:h-180 flex flex-col lg:flex-row items-center justify-between">
     <div className="w-full lg:w-1/2 mb-40">
       <div className="flex justify-center lg:justify-start">
         <p className="w-auto text-5xl text-center text-gray-400 lg:text-2xl mt-50">
@@ -45,7 +45,6 @@ export default function Page() {
           Iphone 14 Pro
         </p>
       </div>
-
       <div>
         <p className="text-center text-3xl text-gray-400 lg:hidden">
           Created to change everything for the better. <br /> For everyone
@@ -60,11 +59,10 @@ export default function Page() {
           Shop Now
         </button>
       </div>
-    </div>
-
+    </div>  
     <div className="w-full lg:w-1/2 flex justify-center lg:justify-end items-end min-w-0 mt-8 lg:mt-0">
       <img
-        className="w-[280px] lg:w-[520px] h-auto object-contain"
+        className="w-150 mb-0 lg:w-[520px]  shrink-0 h-auto object-contain"
         src="Iphone.svg"
         alt=""
       />
@@ -73,57 +71,7 @@ export default function Page() {
 </div>
 
       <div className='flex'>
-        <div className=''>
-          <div className='flex w-250'>
-            {/* ps 5  */}
-          <img className='w-120' src="PlayStation.svg" alt="" />
-            <div className='mt-30'>
-              <p className='text-4xl '>PlayStation 5 </p>
-              <p className='text-2xl text-gray-400 mt-3 '></p>
-            </div>
-            <div className='mt-30 '>
-              <p className='text-4xl'>PlayStation 5 </p>
-              <p className='text-2xl text-gray-400 mt-3'>
-                Incredibly powerful CPUs, GPUs, and an SSD with <br />
-                 integrated I/O will redefine your PlayStation <br /> experience.
-              </p>
-            </div>
-          </div>
-          <div className='flex'>
-          <div className='flex  items-center h-120 bg-[#EDEDED] w-120'>
-          <img className='w-30' src="max.svg" alt="" />
-              <div className=''>
-      <p className='text-3xl ml-20'>Apple</p>
-      <p className='text-3xl ml-20 mt-2'>AirPods</p>
-      <p className='text-3xl ml-20 mt-2'>Max</p>
-                <p className='ml-20 mt-5 text-2xl text-gray-400'>Computational audio. <br /> Listen, it's powerful</p>
-              </div>
-          </div> 
-          <div  className=' h-120 w-130 bg-[#353535]'>
-            <div className='flex items-center'>
-        <img  className='mt-30 w-40' src="vision.svg" alt="" />
-              <div className='mt-20'>
-                    <p className='text-white ml-20 text-3xl'>Apple</p>
-                    <p className='text-white ml-20 text-3xl mt-2'>Vision Pro</p>
-                    <p className='text-2xl text-gray-400 ml-20 mt-3'>An immersive way to <br /> experience <br /> entertainment</p>
-              </div>
-            </div>
 
-                </div>
-          </div>
-               
-        </div>
-      <div className=' bg-[#EDEDED] w-screen flex justify-end'>
-        <div className='flex ml-5 items-center ml-40'>
-            <div className='w-120'>
-          <p className='text-5xl'>MacBook Pro</p>
-          <p className='text-xl mt-3 text-gray-400'>The new 15‑inch MacBook Air makes room for more of<br />what you love with a spacious Liquid Retina display.</p>
-            <button className=' cursor-pointer w-50 h-15 border rounded-2xl text-xl'>Shop Now</button>
-            </div>
-        </div>
-        <img className='w-90 m-0 p-0'  src="MacPro.svg" alt="" />
-      </div>
-          
       </div>
 
 
