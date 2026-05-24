2e2903e443c9d91bbe66e7f2be7ccab3ed6a385a
Danil Kolbasenko <danilvusockij@gmail.com>
2026-05-24 18:33:56 +0300

    Функция для очистки всей корзины

--- src/favorits.js
+++ src/favorits.js
@@ -10,9 +10,8 @@ const [favorits , setFavorits] = useState([])
 
     useEffect(()=>{
         const Favsaved = localStorage.getItem('favorits')
-            if(Favsaved){
-            setFavorits(JSON.parse(Favsaved))
-            }
+            if(Favsaved && Favsaved !== 'undefined')
+         setFavorits( JSON.parse(Favsaved))
     }, [])
 
  useEffect(()=>{
@@ -21,24 +20,26 @@ const [favorits , setFavorits] = useState([])
 
 
     function AddToFavorits(newfavor){
-                setFavorits(favor =>{
+                setFavorits(favor => {
                     const addfav = favor.find(item => item.id === newfavor.id)
                     if(addfav) {
                         return favor.map(item=> 
                             item.id === newfavor.id
-                            ? {...item, quantity: item.quantity + 1}
+                             ? {...item, quantity: item.quantity + 1}
                         : item
                         )
                     } else return [...favor ,{...newfavor , quantity:1}]
                  })    
     }
 
+
+
+    // функция для очитски корзины полностью 
   function deleteFromOrder(delfavor) {
-    setFavorits(favordel => {
-   const index = favordel.findIndex((item) => item.id === delfavor.id)
-    if (index !== -1) order.value.splice(index, 1)
-    })
- 
+        setFavorits(favor => {
+            const index = favor.findIndex(item => item.id !== delfavor.id)
+            if (index !== -1) favor.splice(index, 1)
+            })
   }
     
     return (
