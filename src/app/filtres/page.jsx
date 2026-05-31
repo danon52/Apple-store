3a8ec2002e2dd969a5e9cfdaf2dd9464caf0fdfb
Danil Kolbasenko <danilvusockij@gmail.com>
2026-05-31 10:21:40 +0300

    Добавил выпадающее окно

--- src/app/filtres/page.jsx
+++ src/app/filtres/page.jsx
@@ -1,9 +1,29 @@
+'use client'
+
 import React from 'react'
+import { Slider } from "@/components/ui/slider"
 
 export default function FiltresPAge() {
   return (
     <div>
-      
+        <div>
+            <p className='text-2xl'>Price</p>
+        </div>
+        <div className='mt-5 flex justify-center'>
+            <hr className='text-gray-400 w-90 ' />
+        </div>
+        <div className='mt-10'>
+
+      <Slider
+      defaultValue={[0]}
+      max={100}
+      step={1}
+      className="mx-auto w-full max-w-xs"
+      />
+
+      </div>
+
+
     </div>
   )
 }
