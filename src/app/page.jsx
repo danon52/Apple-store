3dbd9f40f3109b210e8d6923fdd4c4e1e9403889
Danil Kolbasenko <danilvusockij@gmail.com>
2026-05-31 23:27:59 +0300

    адаптация первой страници

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -90,69 +90,139 @@ export default function Page() {
         <div>
 
 
+          <div>
 
-          <div className='lg:flex'>
-
-            <div className=' hidden lg:block  w-235'>
-
-              <div className='  lg:flex  justify-items-center bg-white'>
-                <div className=' w-full h-140  lg:flex justify-items-center '>
-                  <div className='lg:flex ' >
-                    <img className='w-100 lg:hidden' src="SizePlay.svg" alt="" />
-                    <img className='hidden lg:block' src="PlayStation.svg" alt="" />
-                  </div>
-                  <div className='lg:mt-25'>
-                    <div className='flex justify-center '>
-                      <p className=' mt-5  text-5xl lg:text-5xl flex '>PlayStation 5 </p>
+            <div className='lg:flex'>
+              <div className=' hidden lg:block w-240 '>
+                <div className='  lg:flex  justify-items-center bg-white'>
+                  <div className=' w-full  h-120  lg:flex  bg-amber-400 justify-items-center '>
+                    <div className='lg:flex'>
+                      <img className='w-100 lg:hidden' src="SizePlay.svg" alt="" />
+                      <img className='hidden lg:block w-100' src="PlayStation.svg" alt="" />
                     </div>
-                    <div className=''>
-                      <div className=' text-center lg:text-start'>
-                        <p className='text-xl  text-gray-400  lg:text-xl mt-4'>
-                          Incredibly powerful CPUs,GPUs,and an SSD with
-                        </p>
-                        <p className=' text-gray-400'>integrated I/O will redefine your PlayStation experience.  </p>
+                    <div className='lg:mt-25'>
+                      <div className='flex justify-center '>
+                        <p className=' mt-5  text-5xl lg:text-5xl flex '>PlayStation 5 </p>
                       </div>
-                      <p className='hidden lg:text-xl block mt-4 text-gray-400'>
-                        Incredibly powerful CPUs, GPUs, and an SSD with <br /> integrated I/O will redefine your PlayStation <br /> experience.
+                      <div className=''>
+                        <div className=' text-center lg:text-start'>
+                          <p className='text-xl  text-gray-400  lg:text-xl mt-4'>
+                            Incredibly powerful CPUs,GPUs,and an SSD with
+                          </p>
+                          <p className=' text-gray-400'>integrated I/O will redefine your PlayStation experience.  </p>
+                        </div>
+                        <p className='hidden lg:text-xl block mt-4 text-gray-400'>
+                          Incredibly powerful CPUs, GPUs, and an SSD with <br /> integrated I/O will redefine your PlayStation <br /> experience.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <div>
+
+
+                <div className='hidden lg:block bg-amber-200'>
+                  <div className=' w-187 h-150  lg:flex justify-end bg-[#EDEDED]   '>
+                    <div className=' py-15 lg:flex'>
+                      <img className='w-210 lg:hidden' src="MacBookPro14.svg" alt="" />
+                      <div className=' text-center *:lg:mt-25'>
+                        <div className='text-center'>
+                          <div className=' flex justify-center'>
+                            <p className='text-4xl  lg:text-5xl'> MacBook  Pro </p>
+                          </div>
+                        </div>
+                        <div className='  '>
+                          <div className='text-center'>
+                            <p className='text-gray-600 text-[15px]' >
+                              The new 15‑inch MacBook Air makes room for more of
+                            </p>
+                            <p className='text-[15px] text-gray-600'>what you love with a spacious Liquid Retina display. </p>
+                          </div>
+                          <div className=''>
+                            <button className=' cursor-pointer w-85 h-15 mb-20 text-2xl bg-none mt-13  border-2  rounded-2xl '>Shop Now</button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='lg:flex justify-end'>
+                        <img className='hidden lg:block' src="MacPro.svg" alt="" />
+                      </div>
+                    </div>
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+          <div className='hidden lg:flex block  py-0 mt-0 bg-amber-500'>
+            {/* Max  */}
+            <div className='lg:w-125'>
+              <div className='lg:flex'>
+                <div className='w-full h-120  lg:flex justify-items-center bg-[#EDEDED]  '>
+                  <div className='lg:flex'>
+                    <img className=' w-70 lg:hidden ' src="SizeMax.svg" alt="" />
+                    <img className='hidden lg:block w-30' src="max.svg" alt="" />
+                  </div>
+                  <div className='text-center '>
+                    <div className='text-center'>
+                      <div className='flex justify-center mt-3 lg:block text-center '>
+                        <p className='text-5xl block textce lg:hidden'> Apple AirPods Max  </p>
+                        <div className='lg:text-start text-4xl ml-20 mt-15'>
+                          <div>
+                            <p>Apple</p>
+                          </div>
+                          <div>
+                            <p className='py-4'>AirPods</p>
+                          </div>
+                          <p>Max</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='mt-4  flex justify-center'>
+                      <p className='text-gray-400 text-2xl  lg:hidden  ' >
+                        Computational audio. Listen, it's powerful
+                      </p>
+                      <p className='text-gray-400 text-xl  hidden lg:block px-20 text-lg ' >
+                        Computational audio. <br />Listen, it's powerful
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
-
             </div>
 
-            <div className='hidden lg:block ml-40'>
-              <div className=' w-full h-auto   lg: bg-amber-600 bg-[#EDEDED]   '>
+            <div className=' lg:block w-112'>
+              <div className=' w-full h-120   lg:flex justify-items-center bg-[#353535]  '>
                 <div className=' py-15 lg:flex'>
-                  <img className='w-210 lg:hidden' src="MacBookPro14.svg" alt="" />
-                  <div className=' text-center *:lg:mt-25'>
-                    <div className='text-center'>
-                      <div className=' flex justify-center'>
-                        <p className='text-4xl  lg:text-5xl'> MacBook  Pro </p>
+                  <img className='w-100 lg:hidden ' src="SizeVision.svg" alt="" />
+                  <img className='hidden  lg:block w-70' src="vision.svg" alt="" />
+                </div>
+                <div className=' text-center lg:mt-25'>
+                  <div className='text-center'>
+                    <div className=' flex justify-center lg:block'>
+                      <p className='text-5xl  text-white lg:hidden'> Apple Vision Pro </p>
+                      <div className='hidden lg:block ml-10 mt-10'>
+                        <p className='text-5xl  hidden text-white lg:block text-start text-3xl'> Apple  </p>
+                        <p className='text-5xl  hidden text-white lg:block text-start mt-2 text-3xl'> Vision Pro</p>
                       </div>
-                    </div>
-                    <div className='  '>
-                      <div className='text-center'>
-                        <p className='text-gray-600 text-[15px]' >
-                          The new 15‑inch MacBook Air makes room for more of
-                        </p>
-                        <p className='text-[15px] text-gray-600'>what you love with a spacious Liquid Retina display. </p>
-                      </div>
-                      <div className=''>
-                        <button className=' cursor-pointer w-85 h-15 mb-20 text-2xl bg-none mt-13  border-2  rounded-2xl '>Shop Now</button>
-                      </div>
+
                     </div>
                   </div>
-                  <div className='lg:flex justify-end'>
-                    <img className='hidden lg:block' src="MacPro.svg" alt="" />
+                  <div className=' mt-5 text-center '>
+                    <p className='text-gray-400 text-2xl' >
+                      Computational audio. Listen, it's powerful
+                    </p>
                   </div>
                 </div>
-
               </div>
-
             </div>
           </div>
+
 
           <div className=' block lg:hidden '>
             <div className='lg:flex'>
@@ -179,72 +249,6 @@ export default function Page() {
           </div>
           <div>
 
-            <div className=' hidden lg:flex block '>
-              {/* Max  */}
-              <div className='lg:w-120'>
-                <div className='lg:flex'>
-                  <div className='w-full h-120  lg:flex justify-items-center bg-[#EDEDED]  '>
-                    <div className='lg:flex'>
-                      <img className=' w-70 lg:hidden ' src="SizeMax.svg" alt="" />
-                      <img className='hidden lg:block w-30' src="max.svg" alt="" />
-
-                    </div>
-                    <div className='text-center lg:mt-25'>
-                      <div className='text-center'>
-                        <div className='flex justify-center mt-3 lg:block text-center '>
-                          <p className='text-5xl block textce lg:hidden'> Apple AirPods Max  </p>
-                          <div className='lg:text-start text-4xl ml-20 mt-15'>
-                            <div>
-                              <p>Apple</p>
-                            </div>
-                            <div>
-                              <p className='py-4'>AirPods</p>
-                            </div>
-                            <p>Max</p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='mt-4  flex justify-center'>
-                        <p className='text-gray-400 text-2xl  lg:hidden  ' >
-                          Computational audio. Listen, it's powerful
-                        </p>
-                        <p className='text-gray-400 text-xl  hidden lg:block px-20 ' >
-                          Computational audio. <br />Listen, it's powerful
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className=' lg:block w-120 '>
-                <div className=' w-full h-120   lg:flex justify-items-center bg-[#353535]  '>
-                  <div className=' py-15 lg:flex'>
-                    <img className='w-100 lg:hidden ' src="SizeVision.svg" alt="" />
-                    <img className='hidden  lg:block w-70' src="vision.svg" alt="" />
-                  </div>
-                  <div className=' text-center lg:mt-25'>
-                    <div className='text-center'>
-                      <div className=' flex justify-center lg:block'>
-                        <p className='text-5xl  text-white lg:hidden'> Apple Vision Pro </p>
-                        <div className='hidden lg:block ml-10 mt-10'>
-                          <p className='text-5xl  hidden text-white lg:block text-start text-3xl'> Apple  </p>
-                          <p className='text-5xl  hidden text-white lg:block text-start mt-2 text-3xl'> Vision Pro</p>
-                        </div>
-
-                      </div>
-                    </div>
-                    <div className=' mt-5 text-center '>
-                      <p className='text-gray-400 text-2xl' >
-                        Computational audio. Listen, it's powerful
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-            </div>
 
 
 
@@ -303,7 +307,7 @@ export default function Page() {
             </div>
           </div>
 
-          <div className='block mt-10'>
+          <div className='block mt-10 lg:hidden'>
             <div className=' w-full h-auto   lg:flex justify-items-center bg-[#EDEDED]  '>
               <div className=' py-15 lg:flex'>
                 <img className='w-210 lg:w-100' src="MacBookPro14.svg" alt="" />
